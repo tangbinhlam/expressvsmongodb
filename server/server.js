598edb931e9220c6c49a45e0a9a02dbc930b380b
osalam <tangbinhlam@gmail.com>
2020-12-01 10:13:31 -0800

    [HTTP] add post and get method

--- server/server.js
+++ server/server.js
@@ -20,15 +20,54 @@ const employees = [
 ]
 
 const server = http.createServer((req, res) => {
-    res.writeHead(404, {
-        'Content-Type': 'application/json',
-        'X-Powered-By': 'Node.js'
-    });
-    res.end(JSON.stringify({
-        success: false,
-        error: 'Not Found',
-        data: null,
-    }));
+
+    const { method, url } = req;
+    let body = [];
+    req.on('data', chunk => {
+        body.push(chunk);
+    })
+        .on('end', () => {
+            body = Buffer.concat(body).toString();
+
+            let status = 404;
+            let response = {
+                success: false,
+                data: null,
+                error: null
+            }
+
+            if (method === 'GET' && url === '/employees') {
+                status = 200;
+                response = {
+                    ...response,
+                    success: true,
+                    data: employees,
+                }
+            } else if (method === 'POST' && url === '/employees') {
+                const { id, name, age } = JSON.parse(body);
+                if (!id || !name || !age) {
+                    status = 400;
+                    response = {
+                        ...response,
+                        error: 'Please Id | Name | Age',
+                    }
+                } else {
+                    status = 201;
+                    employees.push({ id, name, age });
+                    response = {
+                        ...response,
+                        success: true,
+                        data: employees,
+                    }
+                }
+            }
+
+            res.writeHead(status, {
+                'Content-Type': 'application/json',
+                'X-Powered-By': 'Node.js'
+            });
+            res.end(JSON.stringify(response));
+        })
 });
 
 const PORT = 5000;
