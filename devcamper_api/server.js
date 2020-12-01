b9a2268654f6599aedcac725ee0313824a18bdae
osalam <tangbinhlam@gmail.com>
2020-12-01 15:58:52 -0800

    [MONGODB] Add mongoose and setup connection

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// Load evn vars
+dotenv.config({ path: './config/config.env' });
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
-// Load evn vars
-dotenv.config({ path: './config/config.env' });
+// Connnect to database
+connectDB();
 
 const app = express();
 
@@ -22,6 +26,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
 });
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+})
