1735edc6df3a7908d1b4eca4ad6a984c360feee0
osalam <tangbinhlam@gmail.com>
2020-12-01 11:25:25 -0800

    [API] Create simple API with GET, POST, PUT, DELETE

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -6,6 +6,27 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+// Bootcams - API
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: `Show all bootcamps` });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Show bootcamp with id = ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(201).json({ success: true, msg: `Create bootcamp` });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Update bootcamp  with id = ${req.params.id}` });
+})
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete bootcamp  with id = ${req.params.id}` });
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
