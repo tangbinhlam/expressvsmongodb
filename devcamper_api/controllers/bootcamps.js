23051fc84810eadba2b29a350709d020a7d3ed29
osalam <tangbinhlam@gmail.com>
2020-12-23 10:38:43 -0800

    Cascade Delete

--- devcamper_api/controllers/bootcamps.js
+++ devcamper_api/controllers/bootcamps.js
@@ -118,10 +118,13 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route   DELETE /api/v1/bootcamps/:id
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
     if (!bootcamp) {
         return res.status(400).json({ success: false });
     }
+
+    bootcamp.remove();
+
     res.status(200).json({ success: true, msg: `Delete bootcamp  with id = ${req.params.id}`, data: {} });
 });
 
