986033acb6a84da8e13fd2872b8a398393bdcec7
osalam <tangbinhlam@gmail.com>
2020-12-23 16:09:34 -0800

    update and delete course

--- devcamper_api/controllers/courses.js
+++ devcamper_api/controllers/courses.js
@@ -60,3 +60,43 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
         data: course
     });
 });
+
+// @desc    Update course
+// @route   PUT /api/v1/courses/:id
+// @access  public
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+    let course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    })
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
+
+
+// @desc    Update course
+// @route   DELETE /api/v1/courses/:id
+// @access  public
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+    let course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return next(new ErrorResponse(`No course with the id of ${req.params.id}`), 404);
+    }
+
+    course = await Course.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+        success: true,
+        data: course
+    });
+});
